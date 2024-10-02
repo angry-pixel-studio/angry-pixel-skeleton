65e085b1c1d9c9ad1d2242d986be0929f87e7c4c
Mauro Cristy <maurocristy@gmail.com>
2024-10-02 17:43:17 -0300

    adding github actions

--- src/config/collisionMatrix.ts
+++ src/config/collisionMatrix.ts
@@ -1,5 +1,4 @@
 import { CollisionMatrix } from "angry-pixel";
-import { COLLISION_LAYERS } from "./layers";
 
 export const collisionMatrix = [
     // [COLLISION_LAYERS.SomeLayer, COLLISION_LAYERS.AnotherLayer],
